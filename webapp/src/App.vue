6f806ef45e34168e6684d937e3d460750f3288c0
KanedaAkira91 <goran.petrovic91@googlemail.com>
2023-11-25 02:45:01 +0100

    update

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="app-wrapper">
-      <HeaderVue  v-on:openAvatar-Configurator></HeaderVue>
+      <HeaderVue></HeaderVue>
       <landing-page></landing-page>
     </div>
 </template>
