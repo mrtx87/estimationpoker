6f18001a738f215b78611c6855e90190c73e1823
mrtx87 <patrickbdoerfer@gmail.com>
2023-12-13 18:32:08 +0100

    added impressum

--- webapp/src/components/footer.vue
+++ webapp/src/components/footer.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="footer-wrapper">
-    <div class="footer">
-      <div class="impressum-btn" v-on:click="openImpressumOnClick">Impressum</div>
-      <div class="dsgvo-btn" v-on:click="openDsgvoOnClick">DSGVO</div>
+    <div class="footer-wrapper">
+        <div class="footer">
+            <div class="impressum-btn" v-on:click="openImpressumOnClick">Impressum</div>
+            <div class="dsgvo-btn" v-on:click="openDsgvoOnClick">DSGVO</div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -15,27 +15,26 @@ import Impressum from "@/components/impressum.vue";
 import Dsgvo from "@/components/dsgvo.vue";
 
 export default {
-  name: "FooterVue",
-  components: {
-  },
-  created() {
-    this.appState = useAppStateStore();
-  },
-  data: function () {
-    return {
-      appState: null,
-    }
-  },
-  methods: {
-    openImpressumOnClick: function () {
-      this.appState.setOverlayId(DISPLAY_OVERLAY_STATE.IMPRESSUM);
-      console.log(this.appState._overlayId)
+    name: "FooterVue",
+    components: {},
+    created() {
+        this.appState = useAppStateStore();
     },
-    openDsgvoOnClick: function () {
-      this.appState.setOverlayId(DISPLAY_OVERLAY_STATE.DSGVO);
+    data: function () {
+        return {
+            appState: null,
+        }
     },
-  },
-  computed: {}
+    methods: {
+        openImpressumOnClick: function () {
+            this.appState.setOverlayId(DISPLAY_OVERLAY_STATE.IMPRESSUM);
+            console.log(this.appState._overlayId)
+        },
+        openDsgvoOnClick: function () {
+            this.appState.setOverlayId(DISPLAY_OVERLAY_STATE.DSGVO);
+        },
+    },
+    computed: {}
 };
 </script>
 
@@ -47,6 +46,8 @@ export default {
   width: 100%;
   justify-content: center;
   bottom: 0px;
+  padding: 3px;
+  box-sizing: border-box;
 }
 
 .footer {
@@ -55,7 +56,7 @@ export default {
 }
 
 .impressum-btn {
-  color: #dedede;
+  color: #bbbbbb;
   cursor: pointer;
 }
 
@@ -64,7 +65,7 @@ export default {
 }
 
 .dsgvo-btn {
-  color: #dedede;
+  color: #bbbbbb;
   cursor: pointer;
 }
 
