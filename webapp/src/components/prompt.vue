395bf6dd959e03e257129f0a61a942a2938f9f0f
KanedaAkira91 <goran.petrovic91@googlemail.com>
2023-12-13 16:59:54 +0100

    fixed prompt null handling

--- webapp/src/components/prompt.vue
+++ webapp/src/components/prompt.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="prompt-wrapper">
-        <div class="question-container" v-if="prompt">{{ prompt.question }}</div>
+    <div class="prompt-wrapper" v-if="prompt">
+        <div class="question-container">{{ prompt.question }}</div>
         <div class="prompt-btn-panel">
             <div v-on:click="confirmAction" class="button-activate small-btn">
-                <img v-if="!prompt.crucial " src="../assets/ok_white.svg">
-                <img v-if="prompt.crucial " src="../assets/warn.svg">
+                <img v-if="!prompt?.crucial " src="../assets/ok_white.svg">
+                <img v-if="prompt?.crucial " src="../assets/warn.svg">
                 <span>confirm</span></div>
             <div v-on:click="closeOverlay" class="button-activate small-btn invers">cancel</div>
         </div>
