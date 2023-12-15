13a56539337de7019683e482f62285787fdc95ad
mrtx87 <patrickbdoerfer@gmail.com>
2023-12-15 23:52:59 +0100

    changed deploy script

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -3,11 +3,10 @@
 
 yarn run build
 cp CNAME dist
-cp googlea23fc0c78e9b6234.html dist
 cp 404.html dist
 cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Sektion09/who-the-f_ck-am-i.git master:gh-pages
+git push -f git@github.com:mrtx87/estimationpoker.git main:gh-pages
 
