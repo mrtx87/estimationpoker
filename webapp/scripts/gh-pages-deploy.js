b13315140ce70268da4fe65f360e68dea1fc131b
mrtx87 <patrickbdoerfer@gmail.com>
2023-12-16 01:29:49 +0100

    added folder name to deploy script

--- webapp/scripts/gh-pages-deploy.js
+++ webapp/scripts/gh-pages-deploy.js
@@ -12,6 +12,7 @@ const fs = require("fs");
         const folderName = fs.existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        console.log("FolderName: " + folderName);
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
         await execa("rm", ["-r", folderName]);
